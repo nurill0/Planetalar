0ed735240b311b125bfb5f9d3014c402eb0472ef
nurill0 <nurillod23@mail.ru>
2022-01-21 14:40:37 +0500

    Initial Commit

---
+++ Planetalar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planetalar
+//
+//  Created by Nurillo Domlajonov on 21/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
